b5cc4663169d08b20ff2d9bce56987afa8e0bf36
naveen.reddy <naveen.reddy@dheeyantra.com>
2023-09-06 13:03:24 +0530

    Initial commit

---
+++ deploy-demo/src/test/java/dev/reddy/deploy/demo/DeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.reddy.deploy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
